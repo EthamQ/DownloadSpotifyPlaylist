d1a86bd541d65bc99009d04d37125fb54eb46e40
Raphael Goeldner <raphael.goeldner@gmail.com>
2018-08-06 00:05:36 +0200

    test gitignore

--- backend/provide-values/spotify-auth-info.js
+++ backend/provide-values/spotify-auth-info.js
@@ -2,4 +2,5 @@ exports.spAuthInfo = {
     client_id: '261fe497b68c48208a70d165648d21ea',
     client_secret: '1ff88434fbb8418999eb0e4b9e021316',
     redirect_uri: 'http://localhost:4200'
+    // worked?
 }
